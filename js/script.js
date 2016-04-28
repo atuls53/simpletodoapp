21904f74bf25c500f06158264bec81947a97eb8a
Atul Sethi <atul.sethi@intimetec.com>
2016-04-28 18:25:38 +0530

    Add and Delte Of TODO

--- js/script.js
+++ js/script.js
@@ -3,71 +3,75 @@ To do App
 
 ****/
 
+$(document).ready(function () {
+  get_notes();
+});
 var i = 0;
 var todo = [];
+var trash = [];
 
-function check_todo(){
-	var checkTodo = JSON.parse(localStorage.getItem("todo"));
-	if(checkTodo){
-		i = checkTodo.length;
-		todo = checkTodo;
-		
-	}
-		
-};
-function add_notes(){
-	check_todo();
-	var noteid = i;
-	var notetitle = document.getElementById("notes-title").value;
-	var notedescription = document.getElementById("notes-description").value;
-	
-	todo.push({
-		id: noteid,
-		title:notetitle,
-		style:'',
-		description: notedescription	
-	});
-	localStorage.setItem("todo", JSON.stringify(todo)); 
-		todo = JSON.parse(localStorage.getItem("todo"));
-			
-	document.getElementById("todo_list").innerHTML += "<div class='col-sm-4'><div class='card mb20'><h3>"+todo.slice(-1)[0].title+"</h3><p>" +todo.slice(-1)[0].description+ "</p><span><a href='javascript:void(0)' onclick='moveToTrash("+todo.slice(-1)[0]+");'>Delete</a></span></div></div>";
-	
-	document.getElementById("notes-title").value = "";
-	document.getElementById("notes-description").value = "";
-	
-	
+function check_todo() {
+  var checkTodo = JSON.parse(localStorage.getItem("todo"));
+  if (checkTodo) {
+    i = checkTodo.length;
+    todo = checkTodo;
+
+  }
+
 };
 
-function get_notes(){	
-	var j=0;
-	var getData = JSON.parse(localStorage.getItem("todo"));
-	if(getData){
-		todo = JSON.parse(localStorage.getItem("todo"));
-	for(j=0; j < todo.length ; j++){
-		document.getElementById("todo_list").innerHTML += "<div class='col-sm-4'><div class='card mb20'><h3>"+todo[j].title+"</h3><p>" +todo[j].description+ "</p><span><a href='javascript:void(0)' onclick='moveToTrash("+j+");'>Delete</a></span> <div class='pull-right dropdown'><a href='#' data-target='#' class='dropdown-toggle' data-toggle='dropdown' aria-expanded='false'><i class='fa fa-ellipsis-v' aria-hidden='true'></i></a> <div class='dropdown-menu colors'><a href='javascript:void(0)' onclick='changecolor('red')'></a></div></div></div></div>";
-		
-	}
-	}
+function add_notes() {
+  check_todo();
+  var noteid = i;
+  var notetitle = document.getElementById("notes-title").value;
+  var notedescription = document.getElementById("notes-description").value;
+var date = new Date();
+  todo.push({
+    id: noteid,
+    title: notetitle,
+    time: date.toDateString(),
+    description: notedescription
+  });
+  localStorage.setItem("todo", JSON.stringify(todo));
+  todo = JSON.parse(localStorage.getItem("todo"));
+  get_notes();
+  document.getElementById("notes-title").value = "";
+  document.getElementById("notes-description").value = "";
+
+
 };
-function moveToTrash(id){
-	console.log(todo);
-	console.log(id);
-	
-	var trashedData = todo.splice(todo.indexOf(id), 1);
-	todo.splice(todo.indexOf(id), 1);
+
+function get_notes() {
+  document.getElementById("todo_list").innerHTML = "";
+  var j = 0;
+  var h = "hi";
+  var todo = JSON.parse(localStorage.getItem("todo"));
+  if (todo) {
+    for (j = 0; j < todo.length; j++) {
+      document.getElementById("todo_list").innerHTML += "<div class='col-sm-4'><div class='card mb20'><h3>" + todo[j].title + "</h3><em>Created ON        "+ todo[j].time +" </em><p>" + todo[j].description + "</p><span class='del_todo'><a href='javascript:void(0)' onclick='moveToTrash(" + j + ");'>X</a></span>      <div class='pull-right dropdown'><a href='#' data-target='#' class='dropdown-toggle' data-toggle='dropdown' aria-expanded='false'><i class='fa fa-ellipsis-v' aria-hidden='true'></i></a> <div class='dropdown-menu colors'><a href='javascript:void(0)' onclick='changecolor("+'hi'+");'></a></div></div></div>";
+
+    }
+
+  }
+
+};
+
+function moveToTrash(id) {
+  j = 0;
+  var todo = JSON.parse(localStorage.getItem("todo"));
+  var trashItem = todo.splice(id,1);
+  console.log(trashItem[0]);
+  for(j=0;j < trashItem.length; j++){
+    trash.push(trashItem[j]);
+    
+  }
+   console.log(trash);
   
-	localStorage.setItem("todo", JSON.stringify(todo)); 
-	localStorage.setItem("trashs", JSON.stringify(trashedData)); 
-		
-	console.log(todo);
-	if(!todo.length) {
-			localStorage.removeItem("todo");
-		}
+  localStorage.setItem("todo", JSON.stringify(todo));
+  localStorage.setItem("trash", JSON.stringify(trash));
+  
+  get_notes();
+  if (!todo.length) {
+    localStorage.removeItem("todo");
+  }
 };
-function changeColor(color){
-	console.log("hi");
-};
-
-$( document ).ready(function() {
-get_notes();
-});
